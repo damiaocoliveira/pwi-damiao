a3ae35034c7ed8e4b61b356023e27fe3dc47658e
damiaocoliveira@gmail.com <damcoliveira@gmail.com>
2022-10-26 22:19:25 -0300

    Aula 08 dia 26-10-2022 - primeira aula sobre JS

---
+++ Aula08/olaMundo.js
@@ -0,0 +1,26 @@
+console.log("Olá mundo !!!") // mensagem no java pode ser com aspas simples ou duplas.. 
+// console.log é um comando que vai exibir informações no console do navegador .. 
+// ponto e virgula no java opcional,
+
+var variavel1 = 2;
+// var tem o tipo de escopo global; 
+let variavel2 = "olá"
+// let tem o tipo de escopo local;
+const variavel3 = 3 + 3; 
+// const é do escopo global e não pode ser reescrita; 
+
+console.log("A variavel variavel1 é igual a ", variavel1)
+variavel1 = 4
+
+console.log("Agora a variavel variavel1 é igual a ", variavel1)
+console.log("O valor de variavel2 é igual a", variavel2)
+variavel2 = 5
+
+console.log("Agora o valor de varialvel2 é", variavel2)
+console.log("O valor de variavel2 é igual a", variavel2)
+variavel2 = 'agora vai'
+
+console.log("Agora o valor de varialvel2 é", variavel2)
+console.log("Essa é a constate", variavel3)
+
+
